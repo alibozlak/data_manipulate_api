1f2731c5b34807578cb21821aa4ba1e2df7db79f
bozlak <bozlak20aliihsan@gmail.com>
2026-08-16 20:32:00 +0300

    Edited mistakes

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ mod request_with_json_file;
 async  fn main() {
 
     let app = Router::new()
-        .route("manipulate-datas", post(request_with_json_file::convert_to_json_string))
+        .route("/manipulate-datas", post(request_with_json_file::convert_to_json_string))
         .layer(DefaultBodyLimit::max(32 * 1024 * 1024));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3001").await.unwrap();
